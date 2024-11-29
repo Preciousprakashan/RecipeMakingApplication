bc1492eaa9be333f64dbf93a11391c6c7617a6a7
preciousprakashan <preciousprakashan@gmail.com>
2024-11-29 09:42:50 +0530

    adding recipe page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <ChakraProvider value={defaultSystem} >
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
